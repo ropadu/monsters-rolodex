40e11f29566cb8ce4cb17fa33b904f6f753fc829
Radu Popa <>
2020-04-05 15:35:11 +0300

    Updates

---
+++ precache-manifest.d355863d7bc5bb1eba9eb966f1b18d40.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b753fe83b1506e2387135cbfa03b80d7",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "830b01d3b9b1767bba16",
+    "url": "/monsters-rolodex/static/css/main.e67a8b2f.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "830b01d3b9b1767bba16",
+    "url": "/monsters-rolodex/static/js/main.b55ea36d.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
